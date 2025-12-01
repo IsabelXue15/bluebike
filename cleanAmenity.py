5fe6845662a99d523aae0899f570fe4d00f6444a
IsabelXue15 <isabelxue@college.harvard.edu>
2025-12-01 18:46:47 -0500

    cleans amenity csv to remove dupes

---
+++ cleanAmenity.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+df = pd.read_csv("bluebike_with_landuseAmenity.csv")
+
+dupes = df[df.duplicated(subset="Number", keep=False)]
+
+# Group by 'Number' and concatenate amenities for duplicates
+dfCleaned = df.groupby('Number', as_index=False).agg({
+    'amenity': lambda x: ', '.join(x.dropna().unique()),
+    # Keep other columns from the first occurrence
+    **{col: 'first' for col in df.columns if col not in ['Number', 'amenity']}
+})
+
+print(dfCleaned.head())
+
+dupes = dfCleaned[dfCleaned.duplicated(subset="Number", keep=False)]
+
+dfCleaned.to_csv("bluebike_with_landuseAmenityCleaned.csv", index=False)
+
